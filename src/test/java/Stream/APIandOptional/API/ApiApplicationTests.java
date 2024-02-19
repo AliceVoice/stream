86d4be6649f2aa838e3ddeda557c66ef205516cf
AliceVoice <poctuk22@gmail.com>
2024-02-19 14:16:27 +0300

    initial commit

---
+++ src/test/java/Stream/APIandOptional/API/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Stream.APIandOptional.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
